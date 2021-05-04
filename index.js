631df9374bc13b38f8bf67e0d368087e150c5a95
C-A-Y-D-E <notyetfamousprk@gmail.com>
2021-05-04 12:25:46 +0530

    added more functionality

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const x = require("x-ray-scraper");
-const fetch = require("node-fetch");
+const cron = require("node-cron");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const keywords = ["airdrop", "giveway", "free"];
+const keywords = ["airdrop", "giveaway", "air-dropping"];
 const Chan = require("./model/Chan");
+const TelegramBot = require("node-telegram-bot-api");
 
 dotenv.config({ path: "./config.env" });
+
 mongoose
   .connect(process.env.MONGODB, {
     useNewUrlParser: true,
@@ -15,29 +17,88 @@ mongoose
   .then(() => console.log("Database connected!"))
   .catch((err) => console.log(err));
 
+const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+
 const sentMessage = () => {
   x("https://boards.4channel.org/biz/", ".thread", [
     {
       postId: ".postNum.desktop > a:nth-child(2)",
       subject: ".subject",
       link: "a@href",
+      desp: ".postMessage",
     },
   ]).then(async (obj) => {
     for (let i = 0; i < obj.length; i++) {
       for (let k = 0; k < keywords.length; k++) {
-        if (obj[i]?.subject) {
-          if (obj[i].subject.toLowerCase().indexOf(keywords[k]) !== -1) {
+        if (obj[i]?.desp) {
+          if (obj[i]?.desp?.toLowerCase().indexOf(keywords[k]) !== -1) {
             const post = await Chan.findOne({ postId: obj[i].postId });
             if (post) continue;
-            const apiLink = `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage?chat_id=${process.env.CHAT}&text=${obj[i].link}`;
-            await fetch(apiLink);
+            bot.sendMessage(process.env.CHAT, obj[i].link);
             await Chan.create(obj[i]);
           }
         }
       }
     }
-    process.exit();
   });
 };
 
-sentMessage();
+bot.onText(/\/scan/, (msg, match) => {
+  const chatId = msg.chat.id;
+  x("https://boards.4channel.org/biz/", ".thread", [
+    {
+      postId: ".postNum.desktop > a:nth-child(2)",
+      subject: ".subject",
+      link: "a@href",
+      desp: ".postMessage",
+    },
+  ]).then(async (obj) => {
+    for (let i = 0; i < obj.length; i++) {
+      for (let k = 0; k < keywords.length; k++) {
+        if (obj[i]?.desp) {
+          if (obj[i]?.desp?.toLowerCase().indexOf(keywords[k]) !== -1) {
+            bot.sendMessage(chatId, obj[i].link);
+          }
+        }
+      }
+    }
+  });
+  bot.sendMessage(chatId, "Sending shits");
+});
+
+bot.onText(/\/search/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const input = match.input.split(" ")[1];
+  if (input === undefined) {
+    bot.sendMessage(
+      chatId,
+      "Please provide keywords split by comma ',' or send a single keyword like 'mads-gay'"
+    );
+    return;
+  }
+  const keywords = input.split(",");
+
+  x("https://boards.4channel.org/biz/", ".thread", [
+    {
+      postId: ".postNum.desktop > a:nth-child(2)",
+      subject: ".subject",
+      link: "a@href",
+      desp: ".postMessage",
+    },
+  ]).then(async (obj) => {
+    for (let i = 0; i < obj.length; i++) {
+      for (let k = 0; k < keywords.length; k++) {
+        if (obj[i]?.desp) {
+          if (obj[i]?.desp?.toLowerCase().indexOf(keywords[k]) !== -1) {
+            bot.sendMessage(chatId, obj[i].link);
+          }
+        }
+      }
+    }
+  });
+  bot.sendMessage(chatId, "Sending shits");
+});
+
+cron.schedule("* * * * *", () => {
+  sentMessage();
+});
